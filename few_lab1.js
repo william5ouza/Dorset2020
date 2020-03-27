cb30570e685eb7e24f960cc7db44d1843c99a625
william5ouza <will.dasilva.souza@gmail.com>
2020-03-27 17:29:19 +0000

    ‘commit’

---
+++ few_lab1.js
@@ -0,0 +1,48 @@
+var firstName = 'William';
+var favouriteColor = "Red";
+var favouriteFood  = 'pasta';
+var city = 'Sao Paulo';
+
+var x = 'TIC';
+x = 'tac';
+x = 'toe';
+
+console.log (x);
+console.log()
+
+var x = 'Lauren';
+var y = 'Hardy';
+var z = y;
+y = x;
+x = z;
+console.log(y);
+console.log()
+//the console.log() prints what is inside the brackets
+console.log(firstName);
+console.log()
+console.log(favouriteFood);
+console.log()
+console.log(city);
+console.log()
+// Question 8
+
+var phrase, endingPhrase, one, two, three, four, five, six, seven;
+
+phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+
+endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+
+one =  'Red';
+two = 'orange';
+three = 'yellow';
+four = 'green';
+five = 'blue';
+six = 'indigo ';
+seven = 'violet';
+
+console.log(phrase + ''+''+ endingPhrase);
+console.log()
+
+
+
+
